2992b6ae1cfe05063a5be754e30c030fe913f71a
joangallardo348-collab <joangallardo348@gmail.com>
2026-03-29 17:43:39 -0500

    cambios en ingreso de valores al usuario

--- Estructurafor.c
+++ Estructurafor.c
@@ -2,11 +2,11 @@
 
 int main(){
     int numeroInicial,numeroFinal,valorIncremento;
-    printf("Ingrese un numero Inicial");
+    printf("Ingrese un numero Inicial: ");
     scanf("%i", &numeroInicial);
-    printf("Ingrese un numero Final");
+    printf("Ingrese un numero Final: ");
     scanf("%i", &numeroFinal);
-    printf("Ingrese el valor de Incremento");
+    printf("Ingrese el valor de Incremento: ");
     scanf("%i", &valorIncremento);
     
     int i=0;
