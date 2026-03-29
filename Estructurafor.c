955cbd8b80a784bdc6c2abe4c46a03444e87d793
joangallardo348-collab <joangallardo348@gmail.com>
2026-03-29 17:34:15 -0500

    first commit

---
+++ Estructurafor.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main(){
+    int numeroInicial,numeroFinal,valorIncremento;
+    printf("Ingrese un numero Inicial");
+    scanf("%i", &numeroInicial);
+    printf("Ingrese un numero Final");
+    scanf("%i", &numeroFinal);
+    printf("Ingrese el valor de Incremento");
+    scanf("%i", &valorIncremento);
+    
+    int i=0;
+    for(i=numeroInicial;i<=numeroFinal;i=i+valorIncremento){
+        printf("%i", i);
+    }
+    return 0;
+}
